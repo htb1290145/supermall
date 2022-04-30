1be14acd21475831de4309c93245745781ebaf25
物华 <10572332+wuhua11@user.noreply.gitee.com>
2022-04-30 10:39:56 +0800

    '修改了prettier'

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,7 @@
 /* eslint-disable import/no-duplicates */
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import { Form, FormItem } from 'element-ui'
-import { Input } from 'element-ui'
-// 消息弹框
-import { Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
